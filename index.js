f201808cbe1275701d992b25c61f0a3780b99a18
Khundmir Syed <syedkshf105@gmail.com>
2020-11-14 19:31:32 -0600

    I have tried to connect server with the html page and it was successful

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express')
+const socket = require('socket.io')
+const fs = require('fs')
+const app = express()
+const server = app.listen(3000, () => {
+    console.log("listening to the port: 3000")
+})
+
+app.use(express.static('public'))
+
+const io = socket(server)
+
+io.on('connection', function (socket) {
+    console.log('Hello there we are connected!!', socket.id)
+
+
+})
+
 
